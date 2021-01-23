0fffea433376b44d0b3a92108731e782bc4685d9
Roberto Rosati <robrosit@gmail.com>
2021-01-23 20:15:02 -0300

    Added comments to make_db.py

--- make_db.py
+++ make_db.py
@@ -22,22 +22,33 @@ def make_db(avinput, dbname):
             data = line.strip().split('\t')
             # chr, start, end, ref, alt
             newline = data[:5]
+            # if chromosomes are "chr1", "chr2"... then ANNOVAR will only use the first data column.
+            # if chromosomes are named "1", "2"... then ANNOVAR will use all columns in its output. 
             if newline[0].startswith("chr"):
                 newline[0] = newline[0][3:]
+            # get the first index of genotype data
             if sampleindex is None:
                 for ix, d in enumerate(data):
                     if d.startswith("GT:"):
                         sampleindex = ix+1
                         break
+            # get the total number of samples
             if numsamples is None:
                 numsamples = len(data)-sampleindex
             alt = 0
             for sample in data[sampleindex:]:
                 for count in [sample[0], sample[2]]:
+                    # multiallelic VCFs are not good for us.
+                    if count not in ['0', '1', '.']:
+                        print("Unknown genotype '{}'".format(count))
+                        if count == "2":
+                            print("Are you using a multiallelic VCF? Please split multiallelic lines before using this script.")
+                        print("Aborting script execution.")
+                        sys.exit()
                     alt += {'0':0, '.':0, '1':1}[count]
             # alt fraction
             newline.append('{:.3f}'.format(alt/(2*numsamples)))
-            
+            # "alt alleles / total alleles"
             newline.append('{}/{}'.format(alt, 2*numsamples))
             newline = '\t'.join(newline) + '\n'
             outfile.write(newline)
